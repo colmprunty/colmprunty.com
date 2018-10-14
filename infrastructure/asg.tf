cfac3465a44aa6a27cb67e906a5e5fc84665d62f
ColmPrunty <colmpruntysignups+github@gmail.com>
2018-10-14 14:10:02 +0100

    service discovery work

--- infrastructure/asg.tf
+++ infrastructure/asg.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "homepage_template" {
-  name_prefix = "colmprunty"
-  image_id = "ami-05b65c0f6a75c1c64"
+  name_prefix   = "colmprunty"
+  image_id      = "ami-05b65c0f6a75c1c64"
   instance_type = "t2.micro"
 
   instance_market_options {
@@ -14,11 +14,12 @@ resource "aws_launch_template" "homepage_template" {
 
 resource "aws_autoscaling_group" "homepage_asg" {
   availability_zones = ["eu-west-1c"]
-  desired_capacity = 1
-  max_size = 1
-  min_size = 1
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
   launch_template = {
-    id = "${aws_launch_template.homepage_template.id}"
+    id      = "${aws_launch_template.homepage_template.id}"
     version = "$$Latest"
   }
 }
--- infrastructure/ecs_service.tf
+++ infrastructure/ecs_service.tf
@@ -2,3 +2,42 @@ resource "aws_ecs_cluster" "everything-cluster" {
   name = "everything-cluster"
 }
 
+resource "aws_ecs_service" "colmprunty-com" {
+  name            = "colmprunty-com"
+  cluster         = "${aws_ecs_cluster.everything-cluster.id}"
+  task_definition = "${aws_ecs_task_definition.colmprunty.arn}"
+  desired_count   = 1
+
+  #iam_role        = "${aws_iam_role.ecs-service-role.name}"
+  #depends_on      = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+
+  service_registries {
+    registry_arn   = "${aws_service_discovery_service.colmprunty-sd.arn}"
+    container_port           = 5000
+    container_name = "colmprunty-com"
+  }
+}
+
+resource "aws_service_discovery_service" "colmprunty-sd" {
+  name = "colmprunty-sd"
+
+  dns_config {
+    namespace_id = "${aws_service_discovery_public_dns_namespace.colmprunty-com.id}"
+
+    dns_records {
+      ttl  = 10
+      type = "SRV"
+    }
+  }
+
+  # health_check_config {
+  #   failure_threshold = 10
+  #   resource_path = "path"
+  #   type = "HTTP"
+  # }
+}
+
+resource "aws_service_discovery_public_dns_namespace" "colmprunty-com" {
+  name        = "aws.colmprunty.com"
+  description = "service discovery dns"
+}
