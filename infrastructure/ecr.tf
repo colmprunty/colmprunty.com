0858e6814be6dc90fba42aa8823e22a366ab2945
Colm Prunty <colmpruntysignups+git@gmail.com>
2020-05-08 11:57:34 +0100

    wipe master branch

--- infrastructure/ecr.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "colmprunty-com" {
-  name = "colmprunty-com"
-}
--- infrastructure/taskdefinition.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ecs_task_definition" "colmprunty" {
-  family                = "colmprunty"
-  container_definitions = "${file("./taskdefinition.json")}"
-  network_mode          = "bridge"
-}
